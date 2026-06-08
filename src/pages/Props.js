f1760b7582c76c1689914eaacdd45474e104e9af
tanjil-hosain <tanjilh85@gmail.com>
2026-06-08 12:00:00 +0600

    object_props

--- src/pages/Props.js
+++ src/pages/Props.js
@@ -1,9 +1,20 @@
 import React from 'react'
 
 export default function Props() {
+
+    const person = {
+        name:"Tanjil Hossain", 
+        age: 23,
+        address: "Mirpur"
+    }
   return (
     <>
       <h1>This is my props</h1>
+      <p>
+        Name: {person.name} <br />
+        Age: {person.age} <br />
+        Address: {person.address}
+      </p>
     </>
   )
 }
