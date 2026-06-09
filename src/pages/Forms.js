a2fb8a950bacdbc2b3458e0f58c923d61b3a27c8
tanjil-hosain <tanjilh85@gmail.com>
2026-06-09 13:20:02 +0600

    muliple input

--- src/pages/Forms.js
+++ src/pages/Forms.js
@@ -2,19 +2,34 @@ import React, { useState } from 'react'
 
 
 export default function Forms() {
-  const [name, setName] = useState('');
-  console.log(name);
-   function chnage(e){
-    setName(e.target.value)
+    //single data
+//   const [name, setName] = useState('');
+//   console.log(name);
+//    function chnage(e){
+//     setName(e.target.value)
+//    }
+
+//multiple data
+
+   const [inputs, setInputs] = useState({});
+   function handlechange(e){
+    const name = e.target.name;
+    const value = e.target.value;
+
+    setInputs(values => ({...values, [name]: value}))
    }
+   console.log(inputs)
+
   return (
-    <div>
+    <>
       <h1>Form</h1>
       <form action="">
         Name:
-        <input type="text" name={name} onChange={chnage} />
+  
+        <input type="text" name='fullname' value={inputs.fullname} onChange={handlechange} />
+         <input type="text" name='address' value={inputs.address} onChange={handlechange} />
 
       </form>
-    </div>
+    </>
   )
 }
